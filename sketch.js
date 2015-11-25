e42f62a137001b678fa966c58f3a2abf5901fceb
Paul Klinski <paulklinski@Pauls-Mac.local>
2015-11-25 22:04:48 +0100

    timeline

--- sketch.js
+++ sketch.js
@@ -5,19 +5,26 @@ data = loadJSON("spinalTapDrummers.json");
 
 function setup(){
 
-  var canvas = createCanvas(300,100);
+  var canvas = createCanvas(1000,300);
   canvas.parent('sketch');
+
   var deceasedDrummers = data.deceasedDrummers;
-var x = 20;
-var y = 20;
+  var x = 20;
+  var y = height/2;
+  line (0,y,width,y);
   for(var i = 0; i < deceasedDrummers.length; i++){
     var drummer = deceasedDrummers[i];
     var died = drummer.died;
     var joined = drummer.joined;
+    var sizeFactor = width/70;
+
+    fill(255);
+    // + 1 because one year may also be from jan to dec
+    // the ellipses sizes depend on the length the drummer participated to the band
+    ellipse(x + (1 + died - joined)* sizeFactor/2, y, (1 + died - joined)* sizeFactor, ( 1+ died - joined)* sizeFactor);
+    x = x + (1 + died - joined)* sizeFactor;
 
 
-    ellipse(x, y, died -joined, died -joined);
-    x = x + width/deceasedDrummers.length;
   }
 
 }
